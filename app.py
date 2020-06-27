da70526ccd61eaf02554a1193383e08906e75aa1
Silvio <silviolima@gmail.com>
2020-06-26 22:34:22 -0300

    api com model para regressao incluido

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, jsonify, request
 import pickle
 
 # load model
-filename = "RFmodel.sav"
+filename_rf = "RFmodel.sav"
 model_rf = pickle.load(open(filename,'rb'))
+#
+filename_et = "ExtraTreesRegressor.sav"
+model_et = pickle.load(open(filename,'rb'))
 
 # app
 app = Flask(__name__)
@@ -12,16 +15,6 @@ app = Flask(__name__)
 # routes
 @app.route('/', methods=['POST'])
 
-# get data
-#####data = request.get_json(force=True)
-
-# convert data into dataframe
-####data.update((x, [y]) for x, y in data.items())
-####data_df = pd.DataFrame.from_dict(data)
-####modelo = data_df[0]
-####data_df = data_df.drop(['modelo'], axis=1)
-
-#####print("Modelo:", modelo)
 
 def predict():
     # get data
@@ -33,7 +26,7 @@ def predict():
      
     colunas = data_df.columns
     if ('Classe' in colunas):
-        # predictions
+        # predictions - classification 
         result = model_rf.predict(data_df)
     
         # Linhas acrescentadas pois o modelo preve: Sobreviveu ou Morreu
@@ -43,11 +36,19 @@ def predict():
             status = 1
         else:
             status = 0
-       
-
-    # send back to browser
-    #output = {'results': int(result[0])}
-    output = {'results': int(status)}
+        
+        # send back to browser
+        #output = {'results': int(result[0])}
+        output = {'results': int(status)}
+    
+    elif ('area_total_clean' in colunas):
+        # predictions - regression
+        print("Previsao de valor de venda")
+        #   
+        result = model_et.predict(data_df)
+        # send back to browser
+        #output = {'results': int(result[0])}
+        output = {'results': float64(status)}
 
     # return data
     return jsonify(results=output)
